aa52af424f2e646a96bdd856a9406bb2872cca22
SoccerStud <geekclb@gmail.com>
2014-02-07 17:25:36 -0500

    New link to version checker

--- net/divinerpg/helper/event/UpdateChecker.java
+++ net/divinerpg/helper/event/UpdateChecker.java
@@ -19,7 +19,7 @@ import net.divinerpg.Reference;
 public class UpdateChecker {
 	
 	public static boolean isUpdateAvailable() throws IOException, MalformedURLException {
-            BufferedReader versionFile = new BufferedReader(new InputStreamReader(new URL("https://dl.dropboxusercontent.com/s/xddtt5g4g8xz1ut/DivineRPG1.7Update.txt").openStream()));
+            BufferedReader versionFile = new BufferedReader(new InputStreamReader(new URL("https://raw.github.com/TheSlayerMC/DivineRPG/master/Version.txt").openStream()));
             String curVersion = versionFile.readLine();
 
             versionFile.close();
