5cbd1c80784483e73cf42de8f56ebb0b847c06f8
TheSlayerMC <brock.kerley@hotmail.com>
2014-03-30 10:51:33 +1030

    Cleaned the furnace

--- main/java/net/divinerpg/twilight/blocks/BlockTwilightFurnace.java
+++ main/java/net/divinerpg/twilight/blocks/BlockTwilightFurnace.java
@@ -38,7 +38,7 @@ public class BlockTwilightFurnace extends BlockContainer {
 	private IIcon FurnaceIIconTop;
 	@SideOnly(Side.CLIENT)
 	private IIcon FurnaceIIconFront;
-		
+
 	public BlockTwilightFurnace(boolean active) {
 		super(Material.rock);
 		isActive = active;
@@ -49,7 +49,7 @@ public class BlockTwilightFurnace extends BlockContainer {
 			setCreativeTab(null);
 		}
 	}
-	
+
 	@Override
 	public boolean onBlockActivated(World var1, int var2, int var3, int var4, EntityPlayer player, int var6, float var7, float var8, float var9) {
 		if (!player.isSneaking()) {
@@ -60,46 +60,38 @@ public class BlockTwilightFurnace extends BlockContainer {
 		}
 	}
 
-	public Item func_149650_a(int par1, Random par2Random, int par3)
-	{
+	@Override
+	public Item getItem(World par1, int par2, int par3, int par4) {
 		return Item.getItemFromBlock(TwilightBlocks.twilightFurnace);
 	}
 
-	public void onBlockAdded(World par1World, int par2, int par3, int par4)
-	{
+	@Override
+	public void onBlockAdded(World par1World, int par2, int par3, int par4) {
 		super.onBlockAdded(par1World, par2, par3, par4);
 		this.setDefaultDirection(par1World, par2, par3, par4);
 	}
 
-	private void setDefaultDirection(World par1World, int par2, int par3, int par4)
-	{
-		if (!par1World.isRemote)
-		{
+	private void setDefaultDirection(World par1World, int par2, int par3, int par4) {
+		if (!par1World.isRemote) {
 			Block l = par1World.getBlock(par2, par3, par4 - 1);
 			Block i1 = par1World.getBlock(par2, par3, par4 + 1);
 			Block j1 = par1World.getBlock(par2 - 1, par3, par4);
 			Block k1 = par1World.getBlock(par2 + 1, par3, par4);
 			byte b0 = 3;
 
-			if (l.func_149730_j() && !i1.func_149730_j())
-			{
+			if (l.func_149730_j() && !i1.func_149730_j()) 
 				b0 = 3;
-			}
 
 			if (i1.func_149730_j() && !l.func_149730_j())
-			{
 				b0 = 2;
-			}
+
 
 			if (j1.func_149730_j() && !k1.func_149730_j())
-			{
 				b0 = 5;
-			}
+
 
 			if (k1.func_149730_j() && !j1.func_149730_j())
-			{
 				b0 = 4;
-			}
 
 			par1World.setBlockMetadataWithNotify(par2, par3, par4, b0, 2);
 		}
@@ -107,8 +99,7 @@ public class BlockTwilightFurnace extends BlockContainer {
 
 	@Override
 	@SideOnly(Side.CLIENT)
-	public IIcon getIcon(int par1, int par2)
-	{
+	public IIcon getIcon(int par1, int par2) {
 		return par1 == 1 ? this.FurnaceIIconTop : (par1 == 0 ? this.FurnaceIIconTop : (par1 != par2 ? this.blockIcon : this.FurnaceIIconFront));
 	}
 
@@ -126,61 +117,50 @@ public class BlockTwilightFurnace extends BlockContainer {
 		keepFurnaceInventory = true;
 
 		if (par0)
-		{
 			par1World.setBlock(par2, par3, par4, TwilightBlocks.twilightFurnaceOn);
-		}
+
 		else
-		{
 			par1World.setBlock(par2, par3, par4, TwilightBlocks.twilightFurnace);
-		}
+
 
 		keepFurnaceInventory = false;
 		par1World.setBlockMetadataWithNotify(par2, par3, par4, l, 2);
 
-		if (tileentity != null)
-		{
+		if (tileentity != null) {
 			tileentity.validate();
 			par1World.setTileEntity(par2, par3, par4, tileentity);
 		}
 	}
 
 	@Override
-	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase living, ItemStack item)
-    {
-        int l = MathHelper.floor_double((double)(living.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase living, ItemStack item) {
+		int l = MathHelper.floor_double((double)(living.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
 
-        if (l == 0)
-        {
-            world.setBlockMetadataWithNotify(x, y, z, 2, 2);
-        }
+		if (l == 0)       
+			world.setBlockMetadataWithNotify(x, y, z, 2, 2);
 
-        if (l == 1)
-        {
-            world.setBlockMetadataWithNotify(x, y, z, 5, 2);
-        }
 
-        if (l == 2)
-        {
-            world.setBlockMetadataWithNotify(x, y, z, 3, 2);
-        }
+		if (l == 1)      
+			world.setBlockMetadataWithNotify(x, y, z, 5, 2);
 
-        if (l == 3)
-        {
-            world.setBlockMetadataWithNotify(x, y, z, 4, 2);
-        }
 
-        if (item.hasDisplayName())
-        {
-            ((TileEntityTwilightFurnace)world.getTileEntity(x, y, z)).func_145951_a(item.getDisplayName());
-        }
-    }
-	
-	@SideOnly(Side.CLIENT)
+		if (l == 2)        
+			world.setBlockMetadataWithNotify(x, y, z, 3, 2);
+
+
+		if (l == 3)      
+			world.setBlockMetadataWithNotify(x, y, z, 4, 2);
+
+
+		if (item.hasDisplayName())
+			((TileEntityTwilightFurnace)world.getTileEntity(x, y, z)).func_145951_a(item.getDisplayName());
+
+	}
+
 	@Override
-	public void randomDisplayTick(World par1World, int par2, int par3, int par4, Random par5Random)
-	{
-		if (this.isActive)
-		{
+	@SideOnly(Side.CLIENT)
+	public void randomDisplayTick(World par1World, int par2, int par3, int par4, Random par5Random) {
+		if (this.isActive) {
 			int l = par1World.getBlockMetadata(par2, par3, par4);
 			float f = (float)par2 + 0.5F;
 			float f1 = (float)par3 + 0.0F + par5Random.nextFloat() * 6.0F / 16.0F;
@@ -188,23 +168,19 @@ public class BlockTwilightFurnace extends BlockContainer {
 			float f3 = 0.52F;
 			float f4 = par5Random.nextFloat() * 0.6F - 0.3F;
 
-			if (l == 4)
-			{
+			if (l == 4) {
 				par1World.spawnParticle("smoke", (double)(f - f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
 				par1World.spawnParticle("flame", (double)(f - f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
 			}
-			else if (l == 5)
-			{
+			else if (l == 5) {
 				par1World.spawnParticle("smoke", (double)(f + f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
 				par1World.spawnParticle("flame", (double)(f + f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
 			}
-			else if (l == 2)
-			{
+			else if (l == 2) {
 				par1World.spawnParticle("smoke", (double)(f + f4), (double)f1, (double)(f2 - f3), 0.0D, 0.0D, 0.0D);
 				par1World.spawnParticle("flame", (double)(f + f4), (double)f1, (double)(f2 - f3), 0.0D, 0.0D, 0.0D);
 			}
-			else if (l == 3)
-			{
+			else if (l == 3) {
 				par1World.spawnParticle("smoke", (double)(f + f4), (double)f1, (double)(f2 + f3), 0.0D, 0.0D, 0.0D);
 				par1World.spawnParticle("flame", (double)(f + f4), (double)f1, (double)(f2 + f3), 0.0D, 0.0D, 0.0D);
 			}
@@ -212,40 +188,30 @@ public class BlockTwilightFurnace extends BlockContainer {
 	}
 
 	@Override
-	public void breakBlock(World par1World, int par2, int par3, int par4, Block par5, int par6)
-	{
-		if (!keepFurnaceInventory)
-		{
+	public void breakBlock(World par1World, int par2, int par3, int par4, Block par5, int par6) {
+		if (!keepFurnaceInventory) {
 			TileEntityTwilightFurnace tileentityFurnace = (TileEntityTwilightFurnace)par1World.getTileEntity(par2, par3, par4);
 
-			if (tileentityFurnace != null)
-			{
-				for (int j1 = 0; j1 < tileentityFurnace.getSizeInventory(); ++j1)
-				{
+			if(tileentityFurnace != null) {
+				for(int j1 = 0; j1 < tileentityFurnace.getSizeInventory(); ++j1) {
 					ItemStack itemstack = tileentityFurnace.getStackInSlot(j1);
 
-					if (itemstack != null)
-					{
+					if(itemstack != null) {
 						float f = this.FurnaceRand.nextFloat() * 0.8F + 0.1F;
 						float f1 = this.FurnaceRand.nextFloat() * 0.8F + 0.1F;
 						float f2 = this.FurnaceRand.nextFloat() * 0.8F + 0.1F;
 
-						while (itemstack.stackSize > 0)
-						{
+						while(itemstack.stackSize > 0) {
 							int k1 = this.FurnaceRand.nextInt(21) + 10;
 
-							if (k1 > itemstack.stackSize)
-							{
+							if(k1 > itemstack.stackSize)
 								k1 = itemstack.stackSize;
-							}
 
 							itemstack.stackSize -= k1;
 							EntityItem entityitem = new EntityItem(par1World, (double)((float)par2 + f), (double)((float)par3 + f1), (double)((float)par4 + f2), new ItemStack(itemstack.getItem(), k1, itemstack.getItemDamage()));
 
 							if (itemstack.hasTagCompound())
-							{
 								entityitem.getEntityItem().setTagCompound((NBTTagCompound)itemstack.getTagCompound().copy());
-							}
 
 							float f3 = 0.05F;
 							entityitem.motionX = (double)((float)this.FurnaceRand.nextGaussian() * f3);
@@ -255,37 +221,36 @@ public class BlockTwilightFurnace extends BlockContainer {
 						}
 					}
 				}
-
 				par1World.func_147453_f(par2, par3, par4, par5);
 			}
 		}
-
 		super.breakBlock(par1World, par2, par3, par4, par5, par6);
 	}
-	
+
 	@Override
 	public boolean hasComparatorInputOverride() {
 		return true;
 	}
 
+	@Override
 	public int getComparatorInputOverride(World par1World, int par2, int par3, int par4, int par5) {
 		return Container.calcRedstoneFromInventory((IInventory)par1World.getTileEntity(par2, par3, par4));
 	}
 
+	@Override
 	@SideOnly(Side.CLIENT)
-	public Item func_149694_d(World par1World, int par2, int par3, int par4) {
+	public Item getItemDropped(int par1, Random par2, int par3) {
 		return Item.getItemFromBlock(TwilightBlocks.twilightFurnace);
 	}
-    
-    public Block setName(String name){
-        setBlockName(name);
-        GameRegistry.registerBlock(this, name);
-        return this;
-    }
+
+	public Block setName(String name){
+		setBlockName(name);
+		GameRegistry.registerBlock(this, name);
+		return this;
+	}
 
 	@Override
 	public TileEntity createNewTileEntity(World var1, int var2) {
 		return new TileEntityTwilightFurnace();
 	}
-
 }
